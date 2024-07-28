7c8eeab210e6fc3f7a0b27074df6871f559d8c3d
Amitpatidar587 <138189934+Amitpatidar587@users.noreply.github.com>
2024-07-28 18:46:45 +0530

    work in UI in Scheme and Event Component

--- frontend/src/components/main/Event.js
+++ frontend/src/components/main/Event.js
@@ -22,21 +22,21 @@ export default function Event() {
     
   <div className=' pt-4 pb-3 px-2 mt-1 flex flex-col '>
    <h1 className="text-5xl   text-center inline-block  mx-auto   font-bold text-white drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]"> Upcoming Village Events </h1>
-  <button class='w-16 ml-5 p-1 -mt-10 text-white bg-gradient-to-r from-blue-800  to-purple-800 border-1 hover:scale-110'> add</button>
+  <button class='w-16 ml-5 p-1 rounded-md -mt-10 text-white bg-gradient-to-r from-blue-800  to-purple-800 border-1 hover:scale-110'> add</button>
    
 
-    <div className='flex my-4 pt-5   flex-wrap gap-2 justify-evenly items-center'>
+    <div className='flex my-4 pt-5 flex-wrap gap-2 justify-evenly items-center'>
     {events.map((event)=>(
  
-    <div key={event._id} class="w-80 p-2 m-2 h-3/5 bg-blue-800 rounded-md border-3  text-neutral-300  flex flex-col items-start justify-center gap-3 hover:scale-105  hover:shadow-md hover:shadow-slate-800 transition-shadow">
+    <div key={event._id} class="w-80 p-2 m-4 h-3/5 bg-[#3730a3] rounded-md border-3  text-neutral-300  flex flex-col items-start justify-center gap-3 hover:scale-105  hover:shadow-md hover:shadow-slate-800 transition-shadow">
   <div class="w-full h-1/2 bg-sky-300 rounded-t-md">
     <img className='object-cover rounded-t-md' src={event.image} width='100%' height="100%" />
   </div>
   <div class="pb-3">
-      <p class="px-3 py-1 text-center font-extrabold">{event.title}</p>
-      <p class="px-3 py-1 text-center ">{event.date}</p>
+      <p class="px-3 py-1 text-xl text-center font-extrabold">{event.title}</p>
+      <p class="px-3 py-1 text-green-400  text-center ">{event.date}</p>
       <hr></hr>
-      <p class=" px-3 py-2  overflow-auto h-36">{event.info}</p>
+      <p class=" px-3 py-2 overflow-auto h-36">{event.info}</p>
       
   </div>
  </div>
--- frontend/src/components/main/Scheme.js
+++ frontend/src/components/main/Scheme.js
@@ -27,19 +27,23 @@ export default function Scheme() {
         {" "}
         Upcoming Village Events{" "}
       </h1>
-  <button class='w-16 ml-5 p-1 -mt-10 text-white bg-gradient-to-r from-blue-800  to-purple-800 border-1 hover:scale-110'> add</button>
+  <button class='w-16 ml-5 p-1 rounded-md -mt-10 text-white bg-gradient-to-r from-blue-800  to-purple-800 border-1 hover:scale-110'> add</button>
 
      
    
 
       <div className="flex my-4 pt-5  flex-wrap gap-2 justify-evenly items-center">
         {schemes.map((scheme) => (
-           <div key={scheme._id} class="w-80 p-3 m-2 h-3/5 bg-blue-800 rounded-md border-3  text-neutral-300  flex flex-col items-start justify-center gap-3 hover:scale-105  hover:shadow-md hover:shadow-slate-800 transition-shadow">
-           <span className="text-2xl h-16 overflow-auto font-bold">{scheme.title}</span>
+           <div key={scheme._id} class="w-80 p-4 m-5 h-3/5 bg-[#3730a3] rounded-lg border-3  text-neutral-300  flex flex-col items-start justify-center gap-3 hover:scale-105  hover:shadow-md hover:shadow-slate-800 transition-shadow">
+           <span className="text-2xl w-full justify-center my-auto item-center text-center h-16 overflow-auto font-bold">{scheme.title}</span>
            <div class="pb-3">
-               <p class="px-3 py-1 text-center font-extrabold"><span className="text-xl">Applicable :</span> {scheme.applicableType}</p>
-               <p class="px-3 py-1 fixed-h overflow-auto text-center ">{scheme.details}</p>
+               <p class="px-3 py-3 text-center text-green-400 font-extrabold"><span className="text-xl text-green-400">Applicable : {scheme.applicableType}</span> </p>
                <hr></hr>
+               <p class="px-3 py-2 fixed-h overflow-auto text-center ">{scheme.details}</p>
+               <hr></hr>
+               <br></br>
+               
+               <a href={scheme.registerLink} className="text-xl text-blue-600 text-center p-1 block w-full bg-white font-bold border-2 ">register </a>
             
                
            </div>
